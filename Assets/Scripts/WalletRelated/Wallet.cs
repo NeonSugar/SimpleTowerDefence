5629d8bd3ac92b56fdf5b38ea71bbea83f540e66
Nikita Zakharov <zzxxim@gmail.com>
2024-09-22 19:57:35 +0300

    Added Wallet

---
+++ Assets/Scripts/WalletRelated/Wallet.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using UnityEngine;
+
+namespace SimpleTowerDefence.WalletRelated
+{
+	public class Wallet : MonoBehaviour
+	{
+		[SerializeField] private int _value;
+		public int Value
+		{
+			get => _value;
+			private set
+			{
+				_value = value;
+				ValueChanged?.Invoke(_value);
+			}
+		}
+
+		public Action<int> ValueChanged;
+		public Action<int> Deposited;
+		public Action<int> Withdrawn;
+
+		public static Wallet Instance;
+
+		private void Awake()
+		{
+			if(Instance is not null) throw new ($"{Instance.name} already exists!");
+
+			Instance = this;
+		}
+
+		public void Deposit(int depositAmount)
+		{
+			Value += depositAmount;
+			Deposited?.Invoke(depositAmount);
+		}
+
+		public bool TryWithdraw(int amount)
+		{
+			if(Value < amount) return false;
+
+			Withdraw(amount);
+			return true;
+		}
+
+		private void Withdraw(int withdrawAmount)
+		{
+			Value -= withdrawAmount;
+			Withdrawn?.Invoke(withdrawAmount);
+		}
+	}
+}
---
+++ Assets/Scripts/WalletRelated/WalletRepresentation.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+
+namespace SimpleTowerDefence.WalletRelated
+{
+	public class WalletRepresentation : MonoBehaviour
+	{
+		[SerializeField] private Wallet _wallet;
+		[SerializeField] private TMP_Text _textValue;
+		[SerializeField] private ParticleSystem _depositEffectPrefab;
+		[SerializeField] private Transform _depositEffectParent;
+
+		private void Start()
+		{
+			WhenValueChanged(_wallet.Value);
+			Subscribe();
+		}
+
+		private void OnDestroy()
+		{
+			Unsubscribe();
+		}
+
+		private void Subscribe()
+		{
+			_wallet.ValueChanged += WhenValueChanged;
+			_wallet.Deposited += WhenDeposited;
+		}
+		private void Unsubscribe()
+		{
+			_wallet.ValueChanged -= WhenValueChanged;
+		}
+
+		private void WhenValueChanged(int value)
+		{
+			_textValue.text = $"{value}";
+		}
+		private void WhenDeposited(int _)
+		{
+			Instantiate(_depositEffectPrefab, _depositEffectParent.position, new ());
+		}
+	}
+}
